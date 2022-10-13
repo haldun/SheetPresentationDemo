132f71871a0440ea0214327e9f9389ef09568e07
Haldun Bayhantopcu <haldun@github.com>
2022-10-13 17:02:36 +0200

    foo

--- SheetPresentationDemo/ViewController.swift
+++ SheetPresentationDemo/ViewController.swift
@@ -1,19 +1,69 @@
-//
-//  ViewController.swift
-//  SheetPresentationDemo
-//
-//  Created by Haldun Bayhantopcu on 13.10.22.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(tapped)))
+    }
+
+    @objc private func tapped() {
+        let nav = UINavigationController(rootViewController: MyViewController())
+        present(nav, animated: true)
+    }
+}
+
+final class MyViewController: UIViewController {
+    deinit {
+        print("MyViewController deinit")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemGray4
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(tapped)))
     }
 
+    var colorVisible = false
 
+    @objc private func tapped() {
+
+        if colorVisible {
+//            presentedViewController?.sheetPresentationController?.prefersGrabberVisible = false
+            presentedViewController?.dismiss(animated: true)
+//            dismiss(animated: true)
+            colorVisible = false
+            return
+        }
+
+        let color = ColorViewController()
+        color.isModalInPresentation = true
+        if let sheet = color.sheetPresentationController {
+            sheet.detents = [.medium(), .large()]
+            sheet.largestUndimmedDetentIdentifier = .large
+            sheet.prefersScrollingExpandsWhenScrolledToEdge = false
+            sheet.prefersEdgeAttachedInCompactHeight = true
+            sheet.widthFollowsPreferredContentSizeWhenEdgeAttached = true
+            sheet.prefersGrabberVisible = true
+        }
+        present(color, animated: true)
+
+        colorVisible = true
+    }
 }
 
+final class ColorViewController: UIViewController {
+    deinit {
+        print("deinit")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemMint
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(tapped)))
+    }
+
+    @objc private func tapped() {
+        dismiss(animated: true)
+    }
+}
