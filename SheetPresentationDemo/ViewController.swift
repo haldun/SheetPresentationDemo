75aacbeae17dfcc0f073dceadc80fb9e14624254
Haldun Bayhantopcu <haldun@github.com>
2022-10-14 15:39:23 +0200

    stuff

--- SheetPresentationDemo/ViewController.swift
+++ SheetPresentationDemo/ViewController.swift
@@ -1,60 +1,91 @@
+import Combine
 import UIKit
 
-class ViewController: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .systemBackground
-        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(tapped)))
-    }
-
-    @objc private func tapped() {
-        let nav = UINavigationController(rootViewController: MyViewController())
-        present(nav, animated: true)
-    }
+class Counter {
+    @Published var numberOfColorViewControllerInstances = 0
+    static let shared = Counter()
 }
 
-final class MyViewController: UIViewController {
-    deinit {
-        print("MyViewController deinit")
-    }
+final class ViewController: UIViewController {
+    private var cancellables = Set<AnyCancellable>()
+
+    var colorVisible = false
+    var prefersGrabberVisible = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemGray4
         view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(tapped)))
+        let label = UILabel()
+        label.text = "Number of ColorViewController instances: \(Counter.shared.numberOfColorViewControllerInstances)"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(label)
+
+        let toggle = UISwitch()
+        toggle.translatesAutoresizingMaskIntoConstraints = false
+        toggle.addTarget(self, action: #selector(toggleChanged), for: .primaryActionTriggered)
+        view.addSubview(toggle)
+
+        let descriptionLabel = UILabel()
+        descriptionLabel.text = "prefersGrabberVisible"
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(descriptionLabel)
+
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            toggle.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
+            toggle.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 20),
+            descriptionLabel.centerYAnchor.constraint(equalTo: toggle.centerYAnchor),
+            descriptionLabel.leadingAnchor.constraint(equalTo: toggle.trailingAnchor, constant: 20),
+        ])
+        Counter.shared
+            .$numberOfColorViewControllerInstances
+            .receive(on: DispatchQueue.main)
+            .sink { value in
+                label.text = "Number of ColorViewController instances: \(value)"
+            }
+            .store(in: &cancellables)
     }
 
-    var colorVisible = false
+    @objc private func toggleChanged(toggle: UISwitch) {
+        prefersGrabberVisible = toggle.isOn
+    }
 
     @objc private func tapped() {
-
         if colorVisible {
-//            presentedViewController?.sheetPresentationController?.prefersGrabberVisible = false
             presentedViewController?.dismiss(animated: true)
-//            dismiss(animated: true)
             colorVisible = false
             return
         }
-
         let color = ColorViewController()
-        color.isModalInPresentation = true
         if let sheet = color.sheetPresentationController {
             sheet.detents = [.medium(), .large()]
             sheet.largestUndimmedDetentIdentifier = .large
             sheet.prefersScrollingExpandsWhenScrolledToEdge = false
             sheet.prefersEdgeAttachedInCompactHeight = true
             sheet.widthFollowsPreferredContentSizeWhenEdgeAttached = true
-            sheet.prefersGrabberVisible = true
+            sheet.prefersGrabberVisible = prefersGrabberVisible
         }
         present(color, animated: true)
-
         colorVisible = true
     }
 }
 
 final class ColorViewController: UIViewController {
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        Counter.shared.numberOfColorViewControllerInstances += 1
+    }
+
+    @available(*, unavailable)
+    required init?(coder _: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     deinit {
-        print("deinit")
+        print("ColorViewController deinit")
+        Counter.shared.numberOfColorViewControllerInstances -= 1
     }
 
     override func viewDidLoad() {
